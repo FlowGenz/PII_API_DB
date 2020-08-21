5f89c2c26c9cd3a7d29282228f785dd5c971b4a3
JorisZonowatnik <48790485+JorisZonowatnik@users.noreply.github.com>
2020-08-21 20:07:48 +0200

    fix migration

--- API_DbAccess/API_DbAccess/PII_DBContext.cs
+++ API_DbAccess/API_DbAccess/PII_DBContext.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace API_DbAccess
 {
@@ -26,19 +27,28 @@ namespace API_DbAccess
         public virtual DbSet<SentencesOfTheDay> SentencesOfTheDay { get; set; }
         public virtual DbSet<User> User { get; set; }
 
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-    
+                /*IConfigurationRoot configuration = new ConfigurationBuilder()
+                    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+                optionsBuilder.UseSqlServer(configuration.GetConnectionString("DressDatabase"));*/
             }
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            builder.Entity<Dress>().Property(p => p.RowVersion).IsConcurrencyToken();
-            builder.Entity<User>().Property(p => p.RowVersion).IsConcurrencyToken();
-            builder.Entity<DressOrder>().Property(p => p.RowVersion).IsConcurrencyToken();
+            builder.Entity<Dress>().HasKey(d => d.Id);
+            builder.Entity<DressOrder>().HasKey(d => d.Id);
+            builder.Entity<OrderLine>().HasKey(o => o.Id);
+            builder.Entity<Favorites>().HasKey(f => f.Id);
+            //A supprimer?
+            builder.Entity<User>().HasKey(u => u.Id);
+            builder.Entity<SentencesOfTheDay>().HasKey(s => s.Id);
             base.OnModelCreating(builder);
         }
 
